5e61c282a7d5ccc34caf8239c4ec13d56caca917
jayson07 <jayelegant007@gmail.com>
2023-08-13 12:58:30 -0700

    Data Structures: Lists, Tuples

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def print_list_integer(my_list=[]):
+    """prints all integer of a list"""
+
+    for i in my_list:
+        print(str.format("{}", i))
---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+# 2-replace_in_list.py
+
+
+def replace_in_list(my_list, idx, element):
+    """"replaces an element of a list at a specific position"""
+    if idx < 0:
+        return my_list
+    elif idx >= len(my_list):
+        return my_list
+    else:
+        my_list[idx] = element
+        return my_list
