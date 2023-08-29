b8081236eba123bfa7ca4ef80ca8cab0651a6a59
Jayson07 <jayelegant007@gmail.com>
2023-08-29 01:42:17 -0700

    update

---
+++ 0x05-python-exceptions/Python.h
@@ -0,0 +1,11 @@
+#ifndef _PYTHON_H_
+#define _PYTHON_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
+void print_python_float(PyObject *p);
+
+#endif
